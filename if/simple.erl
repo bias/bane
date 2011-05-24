aadd3dc64e0a41c81aeaf326cd0bcafe76b4fc87
bias <trevor.bain@gmail.com>
2011-05-23 22:50:41 -0500

    Removed timer in if/cell

--- if/simple.erl
+++ if/simple.erl
@@ -1,25 +1,24 @@
 -module(simple).
--export([start/0, start_cell/3, cell/3, start_signal/2, signal/2]).
+-export([start/0, start_cell/2, cell/3, start_signal/2, signal/2]).
 
 start() ->
-	CellPID = start_cell(self(), 60, 60),
+	CellPID = start_cell(self(), 60),
 	{CellPID, start_signal(CellPID, 60)}.
 
-start_cell(PID, Freq, Thresh) ->
-	spawn_link(?MODULE, cell, [PID, [Freq, Thresh], 0]).
+start_cell(PID, Thresh) ->
+	spawn_link(?MODULE, cell, [PID, Thresh, 0]).
 
-cell(PID, [Freq, Thresh], Count) ->
+cell(PID, Thresh, Count) ->
 	receive
-		{hi, _time} -> cell(PID, [Freq, Thresh], Count+1);
-		stop -> ok
-	after
-		round(1000/Freq) -> 
+		{hi, _time} -> 
+			NCount = Count + 1,
 			if 
-				Count > Thresh -> 
-					PID ! {hi, time()}, cell(PID, [Freq, Thresh], 0);
+				NCount > Thresh -> 
+					PID ! {hi, time()}, cell(PID, Thresh, 0);
 				true ->
-					cell(PID, [Freq, Thresh], Count)
-			end
+					cell(PID, Thresh, NCount)
+			end;
+		stop -> ok
 	end.
 
 start_signal(PID, Freq) ->
