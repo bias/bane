9d0aaa7fdb3239445a4cc02abf925bd6b45efc8d
bias <trevor.bain@gmail.com>
2011-06-15 11:44:41 -0500

    committing working copy before cludging

--- examples/centersurround/hexagonal.erl
+++ examples/centersurround/hexagonal.erl
@@ -1,7 +1,7 @@
 -module(hexagonal).
--export([start/1, stop/0, tofile/1, info/0]).
--export([mathplot/1, father/1, mother_cell/1, cell/5]).
--vsn(0.1).
+-export([start/1, stop/0, info/0]).
+-export([father/1, mother_cell/1, cell/7]).
+-vsn(0.2).
 
 %%
 %% Client interface
@@ -10,24 +10,16 @@
 start(N) ->
 	MPid = spawn_link(?MODULE, mother_cell, [trunc(N*(N-1)/2)]),
 	register(mother, MPid),
-	FPid = spawn_link(?MODULE, father, [[MPid]]),
+	FPid = spawn_link(?MODULE, father, [[{MPid,0,0}]]),
 	register(father, FPid),
 	{MPid, FPid}.
 
-stop() ->
-	father ! {procq, self()},
-	receive
-		{procr, Pidl} -> [ P ! stop || P <- Pidl ]
-	end,
-	father ! stop.
+stop() -> nope.
 	
-tofile(Filename) ->
-	register(tofile, spawn_link(?MODULE, mathplot, [Filename])).
-
 info() ->
-	father ! {sizeq, self()},
+	father ! {size_q, self()},
 	receive
-		{sizer, Size} -> ok
+		{size_r, Size} -> ok
 	end,
 	Size.
 
@@ -35,102 +27,106 @@ info() ->
 %% Backend 
 %%
 
-father(Pidl) ->
+father(FamilyList) ->
 	receive
-		{proc, NPid} -> father(merge(Pidl, [NPid]));
-		{procl, NPidl} -> father(merge(Pidl, NPidl));
-		{procq, RPid} -> RPid ! {procr, Pidl}, father(Pidl);
-		{sizeq, RPid} -> RPid ! {sizer, length(Pidl)}, father(Pidl);
+		{p_data, Pdatatup} -> 
+			father([Pdatatup|FamilyList]);
+		{p_query, RPid} -> 
+			[ RPid ! {p_response, Pdatatup} || Pdatatup <- FamilyList ],
+			father(FamilyList);
+		{size_q, RPid} -> 
+			RPid ! {size_r, length(FamilyList)}, 
+			father(FamilyList);
 		stop -> ok
 	end.
 
 mother_cell(N) -> 
-	PIDL = [ spawn_link(?MODULE, cell, [[self()], null, null, 0, N]) || _X <- lists:seq(1,6) ],
-	father ! {procl, PIDL},
+	PIDL = [ spawn_link(?MODULE, cell, [[self()], null, null, 0, N, Family, 1]) || Family <- lists:seq(1,6) ],
+	[ father ! {p_data, {lists:nth(F,PIDL), F, 1}} || F <- lists:seq(1,6) ], 
 	[ lists:nth(X, PIDL) ! {nbh, [lists:nth(rollover(X-1), PIDL), lists:nth(rollover(X+1), PIDL)]} || X <- lists:seq(1, 6) ],
 	[ lists:nth(X, PIDL) ! {pnb, lists:nth(rollover(X+1), PIDL)} || X <- lists:seq(1, 6) ],
-	neuron(PIDL, null, null, 0, null).
+	neuron(PIDL, null, null, 0, null, 0, 0).
 
-cell(Nbh, PN, PNOP=null, P=0, N) -> 
+cell(Nbh, PN, PNOP=null, P=0, N, Family, Gen) -> 
 	receive
 		{pf, PID} -> 
-			cell(merge([PID], Nbh), PN, PNOP, P+1, N);
+			cell(merge([PID], Nbh), PN, PNOP, P+1, N, Family, Gen);
 		{nbh, NewNbh} -> 
-			cell( NewNbh ++ Nbh, PN, PNOP, P, N);
+			cell( NewNbh ++ Nbh, PN, PNOP, P, N, Family, Gen);
 		{pnb, NewPN} -> 
 			NewPN ! {pf, self()},
-			cell(Nbh, NewPN, PNOP, P, N);
+			cell(Nbh, NewPN, PNOP, P, N, Family, Gen);
 		% XXX we shouldn't be here 
 		{nbhq, PID} ->
 			PID ! {nbhr, self(), Nbh},
-			neuron(Nbh, PN, PNOP, P, null);
+			neuron(Nbh, PN, PNOP, P, null, Family, Gen);
 		stop -> ok
 	end;
 
-cell(Nbh, PN, PNOP=null, P=1, N) -> 
-	CPID = spawn_link(?MODULE, cell, [[self(), PN], null, PN, 0, N-1]),
-	father ! {proc, CPID},
+cell(Nbh, PN, PNOP=null, P=1, N, Family, Gen) -> 
+	CPID = spawn_link(?MODULE, cell, [[self(), PN], null, PN, 0, N-1, Family, Gen+1]),
+	father ! {p_data, {CPID, Family, Gen+1}},
 	receive
 		{pf, PID} -> 
-			neuron(merge([PID, CPID], Nbh), PN, PNOP, P+1, CPID);
+			neuron(merge([PID, CPID], Nbh), PN, PNOP, P+1, CPID, Family, Gen);
 		% XXX pq - prefernce number query, pr - preference number reply
 		{pq, PID} ->
 			PID ! {pr, P, CPID, self()},
-			neuron(merge([PID, CPID], Nbh), PN, PNOP=null, P, CPID);
+			neuron(merge([PID, CPID], Nbh), PN, PNOP=null, P, CPID, Family, Gen);
 		% XXX we shouldn't be here 
 		{nbhq, PID} ->
 			PID ! {nbhr, self(), Nbh},
-			neuron(Nbh, PN, PNOP, P, null);
+			neuron(Nbh, PN, PNOP, P, null, Family, Gen);
 		stop -> ok
 	end;
 
-cell(Nbh, PN=null, PNOP, P, N) ->
+cell(Nbh, PN=null, PNOP, P, N, Family, Gen) ->
 	PNOP ! {pq, self()},
 	receive
 		{pf, PID} ->
-			cell(merge([PID],Nbh), PN, PNOP, P+1, N);
+			cell(merge([PID],Nbh), PN, PNOP, P+1, N, Family, Gen);
 		{pr, 1, _CPID, PID} -> 
 			PID ! {pf, self()},
-			cell(merge([PID], Nbh), PID, PNOP, P, N);
+			cell(merge([PID], Nbh), PID, PNOP, P, N, Family, Gen);
 		{pr, 2, CPID, PID} -> 
 			CPID ! {pf, self()},
-			cell(merge([PID, CPID], Nbh), CPID, PNOP, P, N);
+			cell(merge([PID, CPID], Nbh), CPID, PNOP, P, N, Family, Gen);
 		% XXX we shouldn't be here 
 		{nbhq, PID} ->
 			PID ! {nbhr, self(), Nbh},
-			neuron(Nbh, PN, PNOP, P, null);
+			neuron(Nbh, PN, PNOP, P, null, Family, Gen);
 		stop -> ok
 	end;
 
-cell(Nbh, PN, PNOP, P, 0) -> neuron(Nbh, PN, PNOP, P, null);
+cell(Nbh, PN, PNOP, P, 0, Family, Gen) -> neuron(Nbh, PN, PNOP, P, null, Family, Gen);
 
-cell(Nbh, PN, PNOP, P, N) ->
-	CPID = spawn_link(?MODULE, cell, [[self(), PN], null, PN, 0, N-1]),
-	father ! {proc, CPID},
+cell(Nbh, PN, PNOP, P, N, Family, Gen) ->
+	CPID = spawn_link(?MODULE, cell, [[self(), PN], null, PN, 0, N-1, Family, Gen+1]),
+	father ! {p_data, {CPID,Family,Gen+1}},
 	receive
 		{pf, PID} -> 
-			neuron(merge([PID, CPID], Nbh), PN, PNOP, P+1, CPID);
+			neuron(merge([PID, CPID], Nbh), PN, PNOP, P+1, CPID, Family, Gen);
 		% XXX pq - prefernce number query, pr - preference number reply
 		{pq, PID} ->
 			PID ! {pr, P, CPID, PID},
-			neuron(merge([PID, CPID], Nbh), PN, PNOP, P, CPID);
+			neuron(merge([PID, CPID], Nbh), PN, PNOP, P, CPID, Family, Gen);
 		% XXX we shouldn't be here 
 		{nbhq, PID} ->
 			PID ! {nbhr, self(), Nbh},
-			neuron(Nbh, PN, PNOP, P, CPID);
+			neuron(Nbh, PN, PNOP, P, CPID, Family, Gen);
 		stop -> ok
 	end.
 
-neuron(Nbh, PN, PNOP, P, CPID) ->
+neuron(Nbh, PN, PNOP, P, CPID, Family, Gen) ->
 	receive
 		{pf, PID} -> 
-			neuron(merge([PID], Nbh), PN, PNOP, P+1, CPID);
+			neuron(merge([PID], Nbh), PN, PNOP, P+1, CPID, Family, Gen);
 		{pq, PID} ->
 			PID ! {pr, P, CPID, self()},
-			neuron([PID | Nbh], PN, PNOP, P, CPID);
+			neuron([PID | Nbh], PN, PNOP, P, CPID, Family, Gen);
 		{nbhq, PID} ->
 			PID ! {nbhr, self(), Nbh},
-			neuron(Nbh, PN, PNOP, P, CPID);
+			neuron(Nbh, PN, PNOP, P, CPID, Family, Gen);
 		stop -> ok
 	end.
 
@@ -145,22 +141,3 @@ merge([Head|Tail], L2) ->
 rollover(0) -> 6;
 rollover(7) -> 1;
 rollover(X) -> X.
-
-mathplot(Filename) ->
-	father ! {procq, self()},
-	receive
-		{procr, Pidl} -> ok 
-	end,
-	file:write_file(Filename, io_lib:format("GraphPlot[\n{", [])),
-	[mathgather(Filename, P) || P <- Pidl], 
-	file:write_file(Filename, io_lib:format("}\nVertexLabeling->True,\nDirectedEdges->True,\n]", []), [append]).
-		
-mathgather(Filename, Pid) ->
-	Pid ! {nbhq, self()},
-	receive
-		{nbhr, Pid, Nbh} ->
-			file:write_file(Filename, [ mathedge(Pid, N) || N <- Nbh ], [append]) 			
-	end.
-
-mathedge(P, N) ->
-	io_lib:format("\"~w\" -> \"~w\", ", [P, N]).
--- examples/lens/webcam_lense.c
+++ examples/lens/webcam_lense.c
@@ -1,38 +1,182 @@
 #include <stdio.h>
 
+/* opencv libs */
 #include <cv.h>
 #include <highgui.h>
+
+/* c libs for erlang */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h> 
 
 /* erlang libs */
 #include <erl_interface.h>
 #include <ei.h>
 
-/* int main( int argc, char **argv ) { */
-int main() {
-	CvCapture *capture = 0;
-	IplImage  *frame = 0;
-	int       key = 0;
+int main( int argc, char **argv ) {
+
+	/* ***** *** * OPENCV DEFINES * *** ***** */ 
+	CvCapture *capture;
+	IplImage  *img;
+	int key = 0;
+
+	/* ***** *** * ERLANG DEFINES * *** ***** */ 
+	char *self_addr;
+	char *self_hostname;
+	char *self_nodename;
+	char *cookie;
+	char *self_fullname;
+	char *conn_addr;
+	char *conn_nodename;
+
+	struct in_addr self_iaddr;	/* 32-bit IP number of host */
+	struct in_addr conn_iaddr;	/* 32-bit IP number of host */ 
+
+	int fd;	                                 /* file descriptor of Erlang node */
+	int loop = 1;	                         /* Loop flag */
+	int got;                                 /* Result of receive */
+	unsigned char buf[BUFSIZ];               /* Buffer for incoming message */
+	ErlMessage emsg;                         /* Incoming message */
+	ETERM *pid;
+	int family;
+
+	ETERM **families;
+
+	/* ***** *** * GENERAL DEFINES * *** ***** */
+	int x,y;
+
+	#define SELF_ADDR argv[1]
+	#define SELF_HOSTNAME argv[2]
+	#define SELF_NODENAME argv[3]
+	#define COOKIE argv[4]
+	#define CONN_ADDR argv[5]
+	#define CONN_NODENAME argv[6]
+	
+	/* FIXME this should really use getopt */
+	if ( argc != 7 ) {
+		fprintf(stderr, "Usage %s: self_addr self_hostname self_nodename cookie conn_addr conn_nodename", argv[0]);
+		return 1;
+	}
+
+	/* ***** *** * OPENCV SETUP * *** ***** */ 
 
 	/* initialize camera */
 	capture = cvCaptureFromCAM( 0 );
 
 	/* always check */
 	if ( !capture ) {
-		fprintf( stderr, "Cannot open initialize webcam!\n" );
+		fprintf(stderr, "Cannot initialize webcam!\n" );
 		return 1;
 	}
 
 	/* create a window for the video */
 	cvNamedWindow( "result", CV_WINDOW_AUTOSIZE );
 
-	while( key != 'q' ) {
-		/* get a frame */
-		frame = cvQueryFrame( capture );
-		/* always check */
-		if( !frame ) break;
-		/* display current frame */
-		cvShowImage( "result", frame );
-		/* exit if user press 'q' */
+
+	/* check image for 3 channels */
+	if( !(img = cvQueryFrame( capture )) || img->nChannels != 3) {
+		fprintf(stderr, "Expected 3 channel camera");
+		return 1;
+	}
+
+	/* ***** *** * ERLANG SETUP * *** ***** */ 
+
+	/* Configure cnode (self) */
+	strcpy(self_addr = malloc(strlen(SELF_ADDR)+1), SELF_ADDR);
+	strcpy(self_hostname = malloc(strlen(SELF_HOSTNAME)+1), SELF_HOSTNAME);
+	strcpy(self_nodename = malloc(strlen(SELF_NODENAME)+1), SELF_NODENAME);
+	strcpy(cookie = malloc(strlen(COOKIE)+1), COOKIE);
+
+	self_fullname = malloc(strlen(self_nodename)+strlen(self_addr)+2);
+	strcat(strcat(strcpy(self_fullname, self_nodename), "@"), self_addr);
+
+	/* Configure erlang connection */
+	strcpy(conn_addr = malloc(strlen(CONN_ADDR)+1), CONN_ADDR);
+	strcpy(conn_nodename = malloc(strlen(CONN_NODENAME)+1), CONN_NODENAME);
+
+	/* initialize erl_interface (once only) */
+	erl_init(NULL, 0);  
+
+	/* initialize the connection mechanism */
+	self_iaddr.s_addr = inet_addr( self_addr );
+	conn_iaddr.s_addr = inet_addr( conn_addr );
+	if (erl_connect_xinit(self_hostname, self_nodename, self_fullname, &self_iaddr, cookie, 0) == -1)
+		erl_err_quit("erl_connect_init");
+
+	/* connect to a running Erlang node */
+	if ((fd = erl_xconnect( &conn_iaddr, conn_nodename )) < 0)
+		erl_err_quit("erl_connect");
+
+	/* ***** *** * GRAB NUMBER OF RINGS * *** ***** */
+	/* XXX construct 6 by n array of eterm pointers */
+	/* erl_reg_send(fd, "father", msg) */ 
+	while (loop) {
+		got = erl_receive_msg(fd, buf, BUFSIZ, &emsg);
+		if (got == ERL_TICK) {
+			/* ignore */
+		} else if (got == ERL_ERROR) {
+			loop = 0; /* exit while loop */
+		} else {
+			if (emsg.type == ERL_REG_SEND && strcmp(ERL_ATOM_PTR(erl_element(1, emsg.msg)), "XXXheader") == 0 ) {
+				/* FIXME grab it */
+				/* erl_element(2, emsg.msg) */
+				/* graph order should be 1+6(n(n+1)/2) */
+				/* family size should be n(n+1)/2 */
+				loop = 0;
+			}
+			erl_free_term(emsg.msg);
+		}
+	}
+
+	/* ***** *** * GRAB FAMILY LISTS * *** ***** */
+	/* XXX populate 6 by n array of eterm pointers */
+	/* erl_reg_send(fd, "father", msg) */ 
+	while (loop) {
+		got = erl_receive_msg(fd, buf, BUFSIZ, &emsg);
+		if (got == ERL_TICK) {
+			/* ignore */
+		} else if (got == ERL_ERROR) {
+			loop = 0; /* exit while loop */
+		} else {
+			if (emsg.type == ERL_REG_SEND) {
+				/* message will be 2 tuple {F, Pid} */
+				family = erl_element(1, emsg.msg);
+				pid = erl_element(2, emsg.msg);
+				/* erl_free_term(family); */
+			}
+			erl_free_term(emsg.msg);
+		}
+	}
+
+	/* ***** *** *	LOOP	* *** ***** */ 
+	loop = 1;
+	while ( loop && key != 'q' ) {
+		/* get an image */
+		if( !(img = cvQueryFrame(capture)) ) 
+			break;
+		/* display current image */
+		cvShowImage("result", img);
+
+		/* TODO send image to erlang */
+		/* TODO send red pixels to corr pids */
+		/*
+		for(x = 0; x < img->width; x++) {
+			for(y = 0; y < img->width; y++) {
+				((uchar*)(img->imageData + img->widthStep*y))[x*3]; 
+				((uchar*)(img->imageData + img->widthStep*y))[x*3+1];
+				((uchar*)(img->imageData + img->widthStep*y))[x*3+2];
+			}
+		}
+		*/
+		/* XXX int erl_send(fd, to, msg) */
+		/* XXX 
+		args = erl_format((char*)"[data_man, {~f, ~f, ~f, ~f}]", ch.at(0), ch.at(1), ch.at(2), ch.at(3));
+		erl_rpc_to(fd, (char*)"gen_event", (char*)"notify", args);
+		*/
+
+		/* exit if user presses 'q' */
 		key = cvWaitKey( 1 );
 	}
 
