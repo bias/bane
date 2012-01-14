b053306f30ff2656db14fc1edaa50d225323d029
bias <trevor.bain@gmail.com>
2012-01-14 12:11:12 -0600

    Still restructuring for push button example

--- examples/eye/centersurround/meta.erl
+++
@@ -1,7 +0,0 @@
--module(meta).
--export([eval/2]).
-
-eval(String, Bindings)->
-    {ok, ErlTokens, _EndLocation} = erl_scan:string(String),
-    {ok, ErlAbsForm} = erl_parse:parse_exprs(ErlTokens),
-    {value, _Value, _NewBindings} = erl_eval:exprs(ErlAbsForm, Bindings).
