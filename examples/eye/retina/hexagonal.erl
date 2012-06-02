e3a0236b650baefe416177fb311c67c2913b529f
bias <trevor.bain@gmail.com>
2012-06-01 19:23:06 -0500

    Added Shoes graph viz data collector and changed some style issues

--- examples/eye/retina/hexagonal.erl
+++ examples/eye/retina/hexagonal.erl
@@ -1,12 +1,14 @@
 -module(hexagonal).
 
--export([start/1, stop/0, info/0, morph/0]).
--export([father/2, uncle/1, mother_cell/1, cell/7]).
+% Client
+-export([start/1, stop/0, info/0, data/0, data/1, morph/0]).
+% Backend (don't touch!)
+-export([data_collate/2, father/2, aunt/2, uncle/1, mother_cell/1, cell/7]).
 
 -import(record, [cell/1, start_cell/1]).
 -include("record.hrl").
 
--vsn(0.3).
+-vsn(0.4).
 
 %%
 %% Client interface
@@ -19,6 +21,8 @@ start(N) ->
 	register(father, FPid),
 	UPid = spawn_link(?MODULE, uncle, [[]]),
 	register(uncle, UPid),
+	APid = spawn_link(?MODULE, aunt, [[], 0]),
+	register(aunt, APid),
 	{MPid, FPid}.
 
 stop() -> nope.
@@ -33,9 +37,32 @@ info() ->
 morph() ->
 	father ! {morph, feedforward}.
 
+data() ->
+	Pid = spawn_link(?MODULE, data_collate, [self(), standard_io]),
+	aunt ! {c_q, Pid}.
+
+data(FileName) ->
+	{ok, Io} = file:open(FileName, write),
+	Pid = spawn_link(?MODULE, data_collate, [self(), Io]),
+	aunt ! {c_q, Pid}.
+
 %%
 %% Backend 
 %%
+
+data_collate(RPid, Io) ->
+	receive
+		% NOTE we could recur with N and solve async issue ... (for you future folks)
+		{c_r_start, _N} -> data_collate(RPid, Io);
+		{c_r, {{S,T},N}} -> 
+			io:format(Io, "add_edge({:time=>~p}, {:pid=>'~p'}, {:pid=>'~p'})~n", [N,S,T]), 
+			data_collate(RPid, Io);
+		{c_r, {P,N}} -> 
+			io:format(Io, "add_vertex({:pid=>'~p',:time=>~p})~n", [P,N]), 
+			data_collate(RPid, Io);
+		% XXX let's assume we don't have an asynchronicity issue
+		{c_r_stop} -> ok
+	end.
 
 father(FamilyList, N) ->
 	receive
@@ -58,6 +85,18 @@ father(FamilyList, N) ->
 		stop -> ok
 	end.
 
+aunt(Creation, T) ->
+	receive
+		{p_d, Pid} -> aunt([{Pid,T}|Creation], T+1);
+		{e_d, {SPid,TPid}} -> aunt([{{SPid,TPid},T}|Creation], T+1);
+		{c_q, RPid} -> 
+			% XXX again, deal with async later ...
+			RPid ! {c_r_start, T},
+			[ RPid ! {c_r, Data} || Data <- Creation ],
+			RPid ! {c_r_stop},
+			aunt(Creation, T)
+	end.
+
 uncle(FamilyList) ->
 	receive
 		{p_d, Pdatatup} -> 
@@ -69,25 +108,54 @@ uncle(FamilyList) ->
 			uncle(FamilyList)
 	end.
 
+%
+% Network generation
+%
+
+% Cell Parameters: 
+%	Nbh			neighborhood
+%	PN			preferred neighbor
+%	PNOP		preferred neighbor of parent	
+%	P			number of preferences
+%	N			layer number (distance from center)
+%	Family		family (spiral branch)
+%	Gen			family generation (distance along branch)
+
+% Message command prefixes:
+%
+%	Cells
+%				you have ...
+%	pf			been preferred
+%	pr			a preference number reply
+%	pq			a preference number query
+%	nbh			some new neighbors	
+%	pnb			a new preferred neighbor
+%	nbhq		a neighborhood query
+
 mother_cell(N) -> 
-	PIDL = [ spawn_link(?MODULE, cell, [[self()], null, null, 0, N, Family, 1]) || Family <- lists:seq(1,6) ],
-	[ father ! {p_d, {lists:nth(F,PIDL), F, 1}} || F <- lists:seq(1,6) ], 
-	[ lists:nth(X, PIDL) ! {nbh, [lists:nth(rollover(X-1), PIDL), lists:nth(rollover(X+1), PIDL)]} || X <- lists:seq(1, 6) ],
-	[ lists:nth(X, PIDL) ! {pnb, lists:nth(rollover(X+1), PIDL)} || X <- lists:seq(1, 6) ],
-	neuron(PIDL, null, null, 0, null, 0, 0).
+	PidL = [ spawn_link(?MODULE, cell, [[self()], null, null, 0, N, Family, 1]) || Family <- lists:seq(1,6) ],
+	[ father ! {p_d, {lists:nth(F,PidL), F, 1}} || F <- lists:seq(1,6) ], 
+	aunt ! {p_d, self()},
+	[ aunt ! {p_d, lists:nth(F,PidL)} || F <- lists:seq(1,6) ], 
+	[ aunt ! {e_d, {self(), lists:nth(F,PidL)}} || F <- lists:seq(1,6) ], 
+	[ lists:nth(X, PidL) ! {nbh, [lists:nth(ring(X-1,6), PidL), lists:nth(ring(X+1,6), PidL)]} || X <- lists:seq(1, 6) ],
+	[ lists:nth(X, PidL) ! {pnb, lists:nth(ring(X+1,6), PidL)} || X <- lists:seq(1, 6) ],
+	neuron(PidL, null, null, 0, null, 0, 0).
 
 cell(Nbh, PN, PNOP=null, P=0, N, Family, Gen) -> 
 	receive
-		{pf, PID} -> 
-			cell(merge([PID], Nbh), PN, PNOP, P+1, N, Family, Gen);
-		{nbh, NewNbh} -> 
+		{pf, Pid} -> 
+			aunt ! {e_d, {Pid, self()}},
+			cell(merge([Pid], Nbh), PN, PNOP, P+1, N, Family, Gen);
+		{nbh, NewNbh} ->
+			[ aunt ! {e_d, {self(), Target}} || Target <- NewNbh ],
 			cell( NewNbh ++ Nbh, PN, PNOP, P, N, Family, Gen);
 		{pnb, NewPN} -> 
 			NewPN ! {pf, self()},
 			cell(Nbh, NewPN, PNOP, P, N, Family, Gen);
 		% XXX we shouldn't be here 
-		{nbhq, PID} ->
-			PID ! {nbhr, self(), Nbh},
+		{nbhq, Pid} ->
+			Pid ! {nbhr, self(), Nbh},
 			neuron(Nbh, PN, PNOP, P, null, Family, Gen);
 		% XXX temporary hack to pass along image
 		{t, {pho,Str}, _} ->
@@ -104,18 +172,22 @@ cell(Nbh, PN, PNOP=null, P=0, N, Family, Gen) ->
 	end;
 
 cell(Nbh, PN, PNOP=null, P=1, N, Family, Gen) -> 
-	CPID = spawn_link(?MODULE, cell, [[self(), PN], null, PN, 0, N-1, Family, Gen+1]),
-	father ! {p_d, {CPID, Family, Gen+1}},
+	CPid = spawn_link(?MODULE, cell, [[self(), PN], null, PN, 0, N-1, Family, Gen+1]),
+	father ! {p_d, {CPid, Family, Gen+1}},
+	aunt ! {p_d, CPid},
+	aunt ! {e_d, {self(), CPid}},
 	receive
-		{pf, PID} -> 
-			neuron(merge([PID, CPID], Nbh), PN, PNOP, P+1, CPID, Family, Gen);
+		{pf, Pid} -> 
+			aunt ! {e_d, {self(), Pid}},
+			neuron(merge([Pid, CPid], Nbh), PN, PNOP, P+1, CPid, Family, Gen);
 		% XXX pq - prefernce number query, pr - preference number reply
-		{pq, PID} ->
-			PID ! {pr, P, CPID, self()},
-			neuron(merge([PID, CPID], Nbh), PN, PNOP=null, P, CPID, Family, Gen);
+		{pq, Pid} ->
+			aunt ! {e_d, {self(), CPid}},
+			Pid ! {pr, P, CPid, self()},
+			neuron(merge([Pid, CPid], Nbh), PN, PNOP=null, P, CPid, Family, Gen);
 		% XXX we shouldn't be here 
-		{nbhq, PID} ->
-			PID ! {nbhr, self(), Nbh},
+		{nbhq, Pid} ->
+			Pid ! {nbhr, self(), Nbh},
 			neuron(Nbh, PN, PNOP, P, null, Family, Gen);
 		% XXX temporary hack to pass along image
 		{t, {pho, Str}, _} ->
@@ -134,17 +206,21 @@ cell(Nbh, PN, PNOP=null, P=1, N, Family, Gen) ->
 cell(Nbh, PN=null, PNOP, P, N, Family, Gen) ->
 	PNOP ! {pq, self()},
 	receive
-		{pf, PID} ->
-			cell(merge([PID],Nbh), PN, PNOP, P+1, N, Family, Gen);
-		{pr, 1, _CPID, PID} -> 
-			PID ! {pf, self()},
-			cell(merge([PID], Nbh), PID, PNOP, P, N, Family, Gen);
-		{pr, 2, CPID, PID} -> 
-			CPID ! {pf, self()},
-			cell(merge([PID, CPID], Nbh), CPID, PNOP, P, N, Family, Gen);
+		{pf, Pid} ->
+			aunt ! {e_d, {self(), Pid}},
+			cell(merge([Pid],Nbh), PN, PNOP, P+1, N, Family, Gen);
+		{pr, 1, _CPid, Pid} -> 
+			aunt ! {e_d, {self(), Pid}},
+			Pid ! {pf, self()},
+			cell(merge([Pid], Nbh), Pid, PNOP, P, N, Family, Gen);
+		{pr, 2, CPid, Pid} -> 
+			aunt ! {e_d, {self(), Pid}},
+			aunt ! {e_d, {self(), CPid}},
+			CPid ! {pf, self()},
+			cell(merge([Pid, CPid], Nbh), CPid, PNOP, P, N, Family, Gen);
 		% XXX we shouldn't be here 
-		{nbhq, PID} ->
-			PID ! {nbhr, self(), Nbh},
+		{nbhq, Pid} ->
+			Pid ! {nbhr, self(), Nbh},
 			neuron(Nbh, PN, PNOP, P, null, Family, Gen);
 		% XXX temporary hack to pass along image
 		{t, {pho, Str}, _} ->
@@ -163,19 +239,21 @@ cell(Nbh, PN=null, PNOP, P, N, Family, Gen) ->
 cell(Nbh, PN, PNOP, P, 0, Family, Gen) -> neuron(Nbh, PN, PNOP, P, null, Family, Gen);
 
 cell(Nbh, PN, PNOP, P, N, Family, Gen) ->
-	CPID = spawn_link(?MODULE, cell, [[self(), PN], null, PN, 0, N-1, Family, Gen+1]),
-	father ! {p_d, {CPID,Family,Gen+1}},
+	CPid = spawn_link(?MODULE, cell, [[self(), PN], null, PN, 0, N-1, Family, Gen+1]),
+	father ! {p_d, {CPid,Family,Gen+1}},
+	aunt ! {p_d, CPid},
 	receive
-		{pf, PID} -> 
-			neuron(merge([PID, CPID], Nbh), PN, PNOP, P+1, CPID, Family, Gen);
+		{pf, Pid} -> 
+			aunt ! {e_d, {self(), Pid}},
+			neuron(merge([Pid, CPid], Nbh), PN, PNOP, P+1, CPid, Family, Gen);
 		% XXX pq - prefernce number query, pr - preference number reply
-		{pq, PID} ->
-			PID ! {pr, P, CPID, PID},
-			neuron(merge([PID, CPID], Nbh), PN, PNOP, P, CPID, Family, Gen);
+		{pq, Pid} ->
+			Pid ! {pr, P, CPid, Pid},
+			neuron(merge([Pid, CPid], Nbh), PN, PNOP, P, CPid, Family, Gen);
 		% XXX we shouldn't be here 
-		{nbhq, PID} ->
-			PID ! {nbhr, self(), Nbh},
-			neuron(Nbh, PN, PNOP, P, CPID, Family, Gen);
+		{nbhq, Pid} ->
+			Pid ! {nbhr, self(), Nbh},
+			neuron(Nbh, PN, PNOP, P, CPid, Family, Gen);
 		% XXX temporary hack to pass along image
 		{t, {pho, Str}, _} ->
 			{any, 'visualize@127.0.0.1'} ! {t, {pho, Str}, self()}, neuron(Nbh, PN, PNOP, P, null, Family, Gen);
@@ -190,16 +268,17 @@ cell(Nbh, PN, PNOP, P, N, Family, Gen) ->
 		stop -> ok
 	end.
 
-neuron(Nbh, PN, PNOP, P, CPID, Family, Gen) ->
+neuron(Nbh, PN, PNOP, P, CPid, Family, Gen) ->
 	receive
-		{pf, PID} -> 
-			neuron(merge([PID], Nbh), PN, PNOP, P+1, CPID, Family, Gen);
-		{pq, PID} ->
-			PID ! {pr, P, CPID, self()},
-			neuron([PID | Nbh], PN, PNOP, P, CPID, Family, Gen);
-		{nbhq, PID} ->
-			PID ! {nbhr, self(), Nbh},
-			neuron(Nbh, PN, PNOP, P, CPID, Family, Gen);
+		{pf, Pid} -> 
+			aunt ! {e_d, {self(), Pid}},
+			neuron(merge([Pid], Nbh), PN, PNOP, P+1, CPid, Family, Gen);
+		{pq, Pid} ->
+			Pid ! {pr, P, CPid, self()},
+			neuron([Pid | Nbh], PN, PNOP, P, CPid, Family, Gen);
+		{nbhq, Pid} ->
+			Pid ! {nbhr, self(), Nbh},
+			neuron(Nbh, PN, PNOP, P, CPid, Family, Gen);
 		% XXX temporary hack to pass along image
 		{t, {pho, Str}, _} ->
 			{any, 'visualize@127.0.0.1'} ! {t, {pho, Str}, self()}, neuron(Nbh, PN, PNOP, P, null, Family, Gen);
@@ -222,6 +301,7 @@ merge([Head|Tail], L2) ->
 		false -> merge(Tail, [Head | L2])
 	end.
 
-rollover(0) -> 6;
-rollover(7) -> 1;
-rollover(X) -> X.
+% Multiplicative
+ring(0, N) -> N;
+ring(N+1, N) -> 1;
+ring(X, N) -> X.
